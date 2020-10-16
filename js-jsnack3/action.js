8cc1f653549b1c259e640bddfde606d5084be055
PatriziaCrimi <crimi.patrizia@gmail.com>
2020-10-16 11:25:12 +0200

    JSnacks 3 - Aggiunte due soluzioni alternative (ottimizzazione codice)

--- js-jsnack3/action.js
+++ js-jsnack3/action.js
@@ -8,6 +8,9 @@ const numbers_quantity = 5;
 var numbers_array = [];
 var current_number = 0;
 
+
+// **************** SOLUTION NUMBER 1 - BASIC LOGIC ****************
+
 // *********** Entering Inputs ***********
 for (var i = 0; i < numbers_quantity && !isNaN(current_number); i++) {
   current_number = parseFloat(prompt('Enter a number.'));
@@ -16,8 +19,8 @@ for (var i = 0; i < numbers_quantity && !isNaN(current_number); i++) {
     numbers_array.push(current_number);
   } else {
     // Error message
-    console.log('ERROR. One of the values entered is not a number. Please reload the page.');
-    alert('ERROR. The value entered is not a number. Please reload the page.');
+    console.log('ERROR. One of the values entered is not a number. Press \'OK\' to continue.');
+    alert('ERROR. The value entered is not a number. Press \'OK\' to continue.');
   }
 }
 
@@ -25,12 +28,56 @@ for (var i = 0; i < numbers_quantity && !isNaN(current_number); i++) {
 console.log('The array of numbers is:');
 console.log(numbers_array);
 
-// *********** Print Sum ***********
+// Calculating Sum
 var sum = numbers_array[0];
-
 for (var i = 1; i < numbers_array.length; i++) {
-  var sum = sum + numbers_array[i];
+  sum = sum + numbers_array[i];
 }
-
+// Print output
 console.log('The sum of the numbers in the array is: ' + sum + '.');
 alert('The sum of the numbers you entered is: ' + sum + '.');
+
+
+// ******************** SOLUTION NUMBER 2 - OPTIMIZATION ********************
+
+// *********** Entering Inputs & Calculating Sum ***********
+sum = 0;
+
+for (var i = 0; i < numbers_quantity; i++) {
+  current_number = parseFloat(prompt('Enter a number.'));
+  if (!isNaN(current_number)) {
+    sum = sum + current_number;
+  } else {
+    // Invalid input
+    current_number = 0;
+    console.log('ERROR. One of the values entered is not a number. This value is considered \'0\' by the system.');
+    alert('ERROR. The value you entered is not a number. This value is considered \'0\' by the system.');
+  }
+}
+// Print output
+console.log('The sum of the numbers the user entered is: ' + sum + '.');
+alert('The sum of the numbers you entered is: ' + sum + '.');
+
+
+// ********** SOLUTION NUMBER 3 - INPUT CHECK IN THE CYCLE **********
+
+// *********** Entering Inputs & Calculating Sum ***********
+sum = 0;
+check_input = false;
+
+for (var i = 0; i < numbers_quantity && !check_input; i++) {
+  current_number = parseFloat(prompt('Enter a number.'));
+  if (!isNaN(current_number)) {
+    sum = sum + current_number;
+  } else {
+    check_input = true;
+    // Error message
+    console.log('ERROR. One of the values entered is not a number. Please reload the page.');
+    alert('ERROR. The value entered is not a number. Please reload the page.');
+  }
+}
+// Print Sum
+if (check_input === false) {
+  console.log('The sum of the numbers the user entered is: ' + sum + '.');
+  alert('The sum of the numbers you entered is: ' + sum + '.');
+}
