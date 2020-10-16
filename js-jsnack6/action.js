0212e8a735cc460e8df6d2fb9d3d033cba020e88
PatriziaCrimi <crimi.patrizia@gmail.com>
2020-10-16 13:21:11 +0200

    JSnacks 6 - Aggiunta soluzione alternativa senza array e con variabile booleana sentinella

--- js-jsnack6/action.js
+++ js-jsnack6/action.js
@@ -9,6 +9,8 @@ var counter = 0;
 var current_power_result = 0
 var power_results_array = [];
 
+// **************** SOLUTION NUMBER 1 - WITH ARRAY ****************
+
 // *********** Power calculation ***********
 for (var i = 0; i <= counter; i++) {
   var power_exponent = i;
@@ -19,5 +21,23 @@ for (var i = 0; i <= counter; i++) {
     counter++;
   }
 }
-console.log('The array containing the powers of two is:');
+// Print output
+console.log('The array containing the powers of ' + power_base + ' within the specified maximum number limit of ' + maximum_number + ' is:');
 console.log(power_results_array);
+
+
+// **************** SOLUTION NUMBER 2 - WITHOUT ARRAY ****************
+
+// *********** Power calculation ***********
+var check_limit = true;
+for (var i = 0; i === i && check_limit; i++) {
+  var power_exponent = i;
+  current_power_result = Math.pow(power_base, power_exponent);
+  // Check for power result maximum 1000
+  if (current_power_result < maximum_number) {
+    // Print results
+    console.log(power_base + ' elevated to the power of ' + power_exponent + ' is: ' + current_power_result);
+  } else {
+    check_limit = false;
+  }
+}
