cde699fd9b42b24cbd7d02d0c19494fed3e3b13e
PatriziaCrimi <crimi.patrizia@gmail.com>
2020-10-19 11:01:42 +0200

    JSnacks 6 - Aggiunte due soluzioni: senza array ottimizzata & ciclo while

--- js-jsnack6/action.js
+++ js-jsnack6/action.js
@@ -6,11 +6,11 @@ Stampa le potenze di 2 senza superare il numero 1000.
 const power_base = 2;
 const maximum_number = 1000;
 var counter = 0;
-var current_power_result = 0
+var current_power_result = 0;
 var power_results_array = [];
 
 // **************** SOLUTION NUMBER 1 - WITH ARRAY ****************
-
+console.log('***SOLUTION NUMBER 2 - WITH ARRAY***');
 // *********** Power calculation ***********
 for (var i = 0; i <= counter; i++) {
   var power_exponent = i;
@@ -24,10 +24,11 @@ for (var i = 0; i <= counter; i++) {
 // Print output
 console.log('The array containing the powers of ' + power_base + ' within the specified maximum number limit of ' + maximum_number + ' is:');
 console.log(power_results_array);
+console.log('');
 
 
 // **************** SOLUTION NUMBER 2 - WITHOUT ARRAY ****************
-
+console.log('***SOLUTION NUMBER 2 - WITHOUT ARRAY***');
 // *********** Power calculation ***********
 var check_limit = true;
 for (i = 0; check_limit; i++) {
@@ -40,4 +41,33 @@ for (i = 0; check_limit; i++) {
   } else {
     check_limit = false;
   }
+}
+console.log('');
+
+
+// *********** SOLUTION NUMBER 3 - WITHOUT ARRAY OPTIMIZED ***********
+console.log('***SOLUTION NUMBER 3 - WITHOUT ARRAY OPTIMIZED***');
+// *********** Power calculation ***********
+current_power_result = 0
+for (i = 0; current_power_result < maximum_number; i++) {
+  power_exponent = i;
+  // Print results
+  console.log(power_base + ' elevated to the power of ' + power_exponent + ' is: ' + current_power_result);
+  // Power calculation
+  current_power_result = Math.pow(power_base, power_exponent);
+}
+console.log('');
+
+
+// *********** SOLUTION NUMBER 4 - WHILE LOOP ***********
+console.log('***SOLUTION NUMBER 4 - WHILE LOOP***');
+// *********** Power calculation ***********
+current_power_result = 0;
+power_exponent = 0;
+while (current_power_result < maximum_number) {
+  // Print results
+  console.log(power_base + ' elevated to the power of ' + power_exponent + ' is: ' + current_power_result);
+  // Power calculation
+  current_power_result = Math.pow(power_base, power_exponent);
+  power_exponent++;
 }
